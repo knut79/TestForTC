71abe97bfba245b0013a34a7c4c98f823b4bac8e
knut <kdu@devKnut.ls.no>
2013-02-08 12:57:07 +0100

    initial commit

---
+++ TestForTeamCity/TestForTeamCity.Tests/ProgramTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+
+namespace TestForTeamCity.Tests
+{
+    public class ProgramTests
+    {
+        [Test]
+        public void PrintSomeText_WithText()
+        {
+            var p = new Program();
+            var result = p.PrintSomeText("per");
+            Assert.AreEqual(result,1);
+        }
+    }
+}
---
+++ TestForTeamCity/TestForTeamCity/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForTeamCity
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = new Program().PrintSomeText("hei");
+
+            Console.Read();
+        }
+
+        public int PrintSomeText(string t)
+        {
+            Console.WriteLine(t);
+            return 1;
+
+        }
+    }
+}
